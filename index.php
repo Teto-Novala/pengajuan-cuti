556783f742264d81e42dc900aa7f526f05348408
Teto-Novala <tetocoding@gmail.com>
2025-09-22 05:22:05 +0700

    setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/output.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- container start -->
+    <div>
+        <!-- sidebar start -->
+        <!-- sidebar end -->
+        <!-- main start -->
+        <!-- main end -->
+    </div>
+    <!-- container end -->
+</body>
+</html>
