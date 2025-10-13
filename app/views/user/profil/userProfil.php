701430739fa3c712fac4ae2a04e080a459567ae6
Teto-Novala <tetocoding@gmail.com>
2025-10-14 01:02:05 +0700

    feat: fix user profil

--- app/views/user/profil/userProfil.php
+++ app/views/user/profil/userProfil.php
@@ -16,20 +16,7 @@ include "../../../controllers/user/profil/userProfilController.php"
     <?php require "../../../components/user/sidebar/userSidebar.php" ?>
     <main>
         <div class="container">
-            <div class="container-sidebar">
-                <div class="sidebar-list">
-                    <img src="../../../../public/images/beranda.svg" alt="logo">
-                    <a href="../beranda/userBeranda.php">Beranda</a>
-                </div>
-                <div class="sidebar-list">
-                    <img src="../../../../public/images/form.svg" alt="logo">
-                    <a href="../pengajuan/userPengajuan.php">Form Pengajuan</a>
-                </div>
-                <div class="sidebar-list">
-                    <img src="../../../../public/images/histori.svg" alt="logo">
-                    <a href="">Histori Cuti</a>
-                </div>
-            </div>
+            <?php require "../../../components/user/sidebar-desktop/userSidebarDesktop.php" ?>
             <div class="container-fields">
                 <h1>Profil</h1>
                 <div class="profil-field">
