0a1964a1ece6244059fd812d90a006ff9c18b7a6
Teto-Novala <tetocoding@gmail.com>
2025-10-15 02:45:59 +0700

    feat: add sidebar admin

---
+++ app/components/admin/sidebar-desktop/adminSidebarDesktop.php
@@ -0,0 +1,14 @@
+<div class="container-sidebar">
+    <div class="sidebar-list">
+        <img src="../../../../public/images/beranda.svg" alt="logo">
+        <a href="../beranda/adminrBerandaView.php">Beranda</a>
+    </div>
+    <div class="sidebar-list">
+        <img src="../../../../public/images/form.svg" alt="logo">
+        <a href="../mengelola-cuti/adminMengelolaCutiView.php">Mengelola Cuti</a>
+    </div>
+    <div class="sidebar-list">
+        <img src="../../../../public/images/histori.svg" alt="logo">
+        <a href="../laporan-cuti/adminLaporanCutiView.php">Laporan Cuti</a>
+    </div>
+</div>
--- app/views/admin/beranda/adminBerandaView.php
+++ app/views/admin/beranda/adminBerandaView.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cuti</title>
+    <link rel="stylesheet" href="../../../../public/css/reset.css">
+    <link rel="stylesheet" href="../../../../public/css/user/topbar/userTopbar.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <div>CUTI</div>
-            <div class="hamburger">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </nav>
-    </header>
+    <?php require "../../../components/admin/sidebar/adminSidebar.php" ?>
+    <main>
+        <div class="container">
+            <?php require "../../../components/admin/sidebar-desktop/adminSidebarDesktop.php" ?>
+            <div class="container-main"></div>
+        </div>
+    </main>
+    <script src="../../../../public/js/mobile-nav/mobileNav.js"></script>
 </body>
 </html>
